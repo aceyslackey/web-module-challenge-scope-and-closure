e43458fc1978e56099b6d3e0f639245c3c735602
Asiah Palmisano <asiah.hob@gmail.com>
2021-08-16 16:17:25 -0500

    asiah-palmisano

--- index.js
+++ index.js
@@ -28,12 +28,19 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+
+        counter 1 has a function scope 
+
+
   2. Which of the two uses a closure? How can you tell?
   
+        counter 1 
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
+      
+
 
 // counter1 code
 function counterMaker() {
@@ -62,8 +69,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random()* Math.floor(3))
 }
 
 
@@ -80,18 +87,34 @@ Use the finalScore function below to do the following:
   "Away": 5
 }
 */ 
+// params inningcb .....
+function finalScore(inningcb, num){
+  let away = 0;
+  let home = 0;
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+  for( let i = 0; i < num; i++){
+    away = away + inningcb()
+    home = home + inningcb()
+  }
+  return {
+    Home: home,
+    Away: away
 }
+}
+console.log(finalScore(inning,9))
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+  //inningcb
+  // returning an object with a score for home and away
+function getInningScore(inningcb) {
+  return{
+    Home: inningcb(),
+    Away: inningcb()
+  }
 }
 
 
@@ -136,6 +159,9 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
+
+//  getinningscorecb, scorecb
+//   
 function scoreboard(/* CODE HERE */) {
   /* CODE HERE */
 }
